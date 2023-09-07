06298e083388475544f29d37c5e64bf43785c164
Matheus D <matheusdurigon1@gmail.com>
2023-09-07 14:32:09 -0300

    starting the project

---
+++ firebaseConfig.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBxzKLmoiQGFUUQnmXJefAtFxVGxo1IUVs",
+  authDomain: "compassfinalchallenge.firebaseapp.com",
+  projectId: "compassfinalchallenge",
+  storageBucket: "compassfinalchallenge.appspot.com",
+  messagingSenderId: "590149890995",
+  appId: "1:590149890995:web:f9c9a0dfd857096ce811c6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
